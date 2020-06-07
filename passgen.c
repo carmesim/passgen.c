1d141d0491f114a9a25c6781ed7136a6c8875b2c
Merazi <merazguerra@protonmail.com>
2020-06-07 11:47:17 -0600

    The password generator does work!

--- passgen.c
+++ passgen.c
@@ -7,20 +7,19 @@ void main()
         delay();
         time_t time_var;
         int pass_length;
-        char arr_numbers[] = "123456789";
-        char arr_lettersLowC[] = "abcdefghkjlmnopqrstuvwxyz";
-        char arr_lettersUpC[] = "ABCDEFGHKJLMNOPQRSTUVWXYZ";
-        char arr_symbols[] = "!@#$%%^&*()_+|";
-
-        printf("How many digits do you want your password to be? (integer)\n");
+        char arr_password_material[] = "abcdefghkjlmnopqrstuvwxyzABCDEFGHKJLMNOPQRSTUVWZYZ1234567890!@#$%%^&*()_+|\\=-{}][\"'.><,?";
+        // Options for the password generation
+        printf("How many digits do you want your password to be? ");
         scanf("%d", &pass_length);
 
         srand((unsigned)time(&time_var));
         printf("Your new password: ");
+
         for (int i = 0; i < pass_length; i++)
         {
-                printf("%c", c[rand() % 10]);
+                printf("%c", arr_password_material[rand() % sizeof(arr_password_material)]);
         }
+        printf("\n");
 }
 
 void delay()
@@ -29,7 +28,5 @@ void delay()
            that's why I'm doing a delay, just to make sure that the
            numbers are indeed random */
         for (int c = 1; c <= 31276; c++)
-                for (int d = 1; d <= 5876; d++)
-                {
-                }
+                for (int d = 1; d <= 5876; d++) { }
 }
