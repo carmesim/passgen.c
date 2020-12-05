7e5c600845976069684611631333cb9da5880e92
merazi <merazguerra@protonmail.com>
2020-12-05 12:35:16 -0600

    Simplified the output

--- src/cli.c
+++ src/cli.c
@@ -5,52 +5,55 @@
 
 static inline void print_help()
 {
-    //printf(__BALEIA_USAGE);
-    printf("%-16s\tShow this message and exit.\n", "-h, --help");
-    printf("%-16s\tSet a vocabulary on which passwords are generated from.\n", "-v, --vocab");
-    printf("%-16s\tSet the size of the generated password.\n", "-s, --size");
+  //printf(__BALEIA_USAGE);
+  printf ("%-16s\tShow this message and exit.\n", "-h, --help");
+  printf ("%-16s\tSet a vocabulary on which passwords are generated from.\n", "-v, --vocab");
+  printf ("%-16s\tSet the size of the generated password.\n", "-s, --size");
 }
 
-cli_args_t parse_cli_args(int argc, char ** argv)
+cli_args_t parse_cli_args (int argc, char ** argv)
 {
-    cli_args_t flags_passed = {0, NULL};
-    int i;
-    if(argc == 1) return flags_passed;
-
-    for (i = 1; i < argc; i++)
+  cli_args_t flags_passed = {0, NULL};
+  int i;
+  if (argc == 1)
     {
-        if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help"))
+      return flags_passed;
+    }
+
+  for (i = 1; i < argc; i++)
+    {
+      if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help"))
         {
-            print_help();
-            exit(0);
+	  print_help ();
+	  exit (0);
         }
-        else if (!strcmp(argv[i], "-v") ||!strcmp(argv[i], "--vocab"))
+      else if (!strcmp(argv[i], "-v") ||!strcmp(argv[i], "--vocab"))
         {
-            if (i + 1 >= argc)
+	  if (i + 1 >= argc)
             {
-                fprintf(stderr, "error: missing value to --vocab");
-                exit(0);
+	      fprintf (stderr, "error: missing value to --vocab");
+	      exit (0);
             }
-            flags_passed.vocab = argv[++i];
+	  flags_passed.vocab = argv[++i];
         }
-        else if (!strcmp(argv[i], "-s") || !strcmp(argv[i], "--size"))
+      else if (!strcmp (argv[i], "-s") || !strcmp (argv[i], "--size"))
         {
-            if (i + 1 >= argc)
+	  if (i + 1 >= argc)
             {
-                fprintf(stderr, "error: missing value to --size");
-                exit(0);
+	      fprintf (stderr, "error: missing value to --size");
+	      exit (0);
             }
-            flags_passed.size = atoi(argv[++i]);
-            if (flags_passed.size <= 0) {
-                fprintf(stderr, "spg: invalid value for size.\n");
-                exit(0);
-            }
+	  flags_passed.size = atoi(argv[++i]);
+	  if (flags_passed.size <= 0) {
+	    fprintf (stderr, "spg: invalid value for size.\n");
+	    exit (0);
+	  }
         }
-        else
+      else
         {
-            fprintf(stderr, "spg: unknown option %s", argv[i]);
-            exit(0);
+	  fprintf (stderr, "spg: unknown option %s", argv[i]);
+	  exit (0);
         }
     }
-    return flags_passed;
+  return flags_passed;
 }
--- src/main.c
+++ src/main.c
@@ -4,27 +4,30 @@
 #include <time.h>       // time_t
 #include <unistd.h>     // sleep
 
-void generate_password(cli_args_t opts)
-{
-        size_t i = 0;
-        const char arr_password_material[] = "abcdefghkjlmnopqrstuvwxyzABCDEFGHKJLMNOPQRSTUVWZYZ1234567890!@#$%%^&*()_+|\\=-{}][\"'.><,?";
+const char arr_password_material [] = "abcdefghkjlmnopqrstuvwxyzABCDEFGHKJLMNOPQRSTUVWZYZ1234567890!@#$%%^&*()_+|\\=-{}][\"'.><,?";
 
-        printf("Please copy your new password: \033[0;32m");
-        for (; i < opts.size; i++)
-                printf("%c", arr_password_material[rand() % sizeof(arr_password_material)]);
-        printf("\n\e[0m");
+void generate_password (cli_args_t opts)
+{
+  size_t i = 0;
+  for (; i < opts.size; i++)
+    {
+      printf ("%c", arr_password_material [rand () % sizeof (arr_password_material)]);
+    }
+  printf ("\n");
 }
 
-int main(int argc, char ** argv) {
-    time_t time_var;
-    cli_args_t opts = parse_cli_args(argc, argv);
-    if (!opts.size) {
-        printf("Type in the size of the password to be generated: ");
-        scanf("%lu", &opts.size);
+int main (int argc, char ** argv)
+{
+  time_t time_var;
+  cli_args_t opts = parse_cli_args (argc, argv);
+  if (!opts.size)
+    {
+      printf ("Type in the size of the password to be generated: ");
+      scanf ("%lu", &opts.size);
     }
-    srand((unsigned)time(&time_var));
+  srand ((unsigned) time (&time_var));
 
-    generate_password(opts);
-    usleep(150);
-    return 0;
+  generate_password (opts);
+  usleep (150);
+  return 0;
 }
